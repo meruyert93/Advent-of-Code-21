517de60c5633c4d25ae56ecfaeb8f466d56a54c8
meruyert93 <61665812+meruyert93@users.noreply.github.com>
2021-12-01 19:00:44 +0100

    implement solution to day 1

--- src/days/01/index.ts
+++ src/days/01/index.ts
@@ -1,66 +1,39 @@
 import { toInt } from "../../lib/helpers";
 
-const TARGET = 2020;
-// all gas, too many breaks
-
-export function one(_input: string[]): number {
+export function one(_input: string[]): number{
   const input = _input.map(toInt);
-  input.sort((a, b) => a - b);
 
-  let x = 0;
+  let sum: number = 0;
+  
+  for (let i = 1; i < input.length; i++) {
+    const firstVal = input[i]
 
-  for (let i = 0; i < input.length; i += 1) {
-    const firstVal = input[i];
-    const offset = TARGET - firstVal;
+    const secondVal = input[i-1];
 
-    for (let j = input.length - 1; j >= 0; j -= 1) {
-      const secondVal = input[j];
-
-      if (firstVal + secondVal === TARGET) {
-        x = firstVal * secondVal;
-        return x;
-        // optimization: iterate backwards for more likely early matches
-      } else if (secondVal < offset) {
-        break;
-      }
+    if (firstVal - secondVal > 0) {
+      sum++;
     }
+
   }
 
-  return x;
+  return sum;
 }
 
-export function two(_input: string[]): number {
+export function two(_input: string[]): number{
   const input = _input.map(toInt);
-  input.sort((a, b) => a - b);
 
-  let x = 0;
+  let result: number;
 
-  for (let i = 0; i < input.length; i += 1) {
-    const firstVal = input[i];
-
-    for (let j = 0; i < input.length; j += 1) {
-      const secondVal = input[j];
-      const intermediarySum = firstVal + secondVal;
-
-      // optimization: break early when branch will never match
-      if (intermediarySum > TARGET) {
-        break;
-      }
-
-      for (let k = 0; k < input.length; k += 1) {
-        const thirdVal = input[k];
-        const sum = intermediarySum + thirdVal;
-
-        if (sum === TARGET) {
-          x = firstVal * secondVal * thirdVal;
-          return x;
-          // optimization: break early when branch will never match
-        } else if (sum > TARGET) {
-          break;
-        }
-      }
+  let sumArr: Array<number> = []
+  
+    for (let i = 2; i < input.length; i++) {
+      sumArr.push(input[i-2]+input[i-1]+input[i])
     }
-  }
 
-  return x;
+    const newInput = sumArr.map(el => String(el));
+    
+    result = one(newInput);
+    return result;
 }
+
+  
