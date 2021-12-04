111422872971868d4042147841fe26266cb92030
meruyert93 <61665812+meruyert93@users.noreply.github.com>
2021-12-04 12:25:16 +0100

    day 03 solution

--- src/days/03/index.ts
+++ src/days/03/index.ts
@@ -1,32 +1,78 @@
-function treeCounter(input: string[], step: number, downStep: number): number {
-  const gridSize = input[0].length;
+import { toInt } from "../../lib/helpers";
 
-  let offset = 0;
 
-  let treeCount = 0;
+export function one(_input: string[]): number {
+  const itemLength = _input[0].length;
 
-  for (let i = 0; i < input.length; i += downStep) {
-    const current = input[i][offset];
-    if (current === "#") {
-      treeCount += 1;
+  //Creating new array that will count '1' in each column
+  const counterArr = new Array(itemLength).fill(0);
+
+  //Calculating number of occurance of '1' in each column
+  for (let i = 0; i < _input.length; i++) {
+
+    for (let j = 0; j < itemLength; j++) {
+      if (_input[i][j] === '1') {
+        counterArr[j]++
+      }
     }
-    offset =
-      offset + step < gridSize ? offset + step : offset + step - gridSize;
   }
 
-  return treeCount;
+  // if number of '1' is more than half length of input array, it means it occured the most in this column
+  const gammaRateBinary = counterArr.map(el => el > _input.length/2 ? '1' : '0').join('');
+
+  // transforming the result of gammaRate to obtain the epsilonRate
+  const epsilonRateBinary = gammaRateBinary
+                            .split('')
+                            .map(el => String(1-toInt(el))).join('')
+
+  const gammaRateDecimal = parseInt(gammaRateBinary, 2);
+  const epsilonRateDecimal = parseInt(epsilonRateBinary, 2)
+  
+  return gammaRateDecimal*epsilonRateDecimal;
 }
 
-export function one(input: string[]): number {
-  return treeCounter(input, 3, 1);
+const getRating = (input: string[], type: string) => {
+  
+  let index: number = 0;
+
+  while (input.length > 1) {
+    /** here we need to find how many time occured "1" in the 'index' column
+     *  then assign the number to counter
+     */
+    let counter: number = 7;
+
+    /** Then, by using 'counter, we identify whether "1" or "0" occured most in the "index" column.
+     *  After that, we assign that value to the variable mostOccuredBit
+     */
+    let mostOccuredBit: string = '1';
+
+    let filterVal: string;
+
+    /** If the type is oxygen, filterVal will be mostOccuredBit,
+     *  if the type is co0, filterVal will be 1 - mostOccuredBit
+     */
+    if (type === 'oxygen') {
+      filterVal =  mostOccuredBit;
+    } else if (type === 'co2') {
+      filterVal = String(1 - toInt(mostOccuredBit))
+    }
+
+    /** then, input array will be just filtered based on 
+     * filterVal in the corresponding index of the item
+     */
+    input = input.filter(el => el[index] === filterVal)
+
+    //increase the index to make calculation for the next column
+    index++;
+  }
+
+  return input[0]
 }
 
-export function two(input: string[]) {
-  return (
-    treeCounter(input, 1, 1) *
-    treeCounter(input, 3, 1) *
-    treeCounter(input, 5, 1) *
-    treeCounter(input, 7, 1) *
-    treeCounter(input, 1, 2)
-  );
+
+export function two(_input: string[]): number {
+  const oxygenRating = getRating(_input, 'oxygen');
+  const co2Rating = getRating(_input, 'co2');
+
+  return parseInt(oxygenRating, 2) * parseInt(co2Rating, 2);
 }
